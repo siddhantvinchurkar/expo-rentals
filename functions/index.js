4f6b04d11ad197c540f958a82cf7502a88af508b
Siddhant <siddhantvinchurkar@gmail.com>
2020-01-04 16:24:08 +0530

    Test 1

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.addEmail = functions.https.onRequest((request, response) => {
+	var email = request.query.email;
+	cors((request, response) => {
+		response.status(200).send(email);
+	});
+	return true;
+});
